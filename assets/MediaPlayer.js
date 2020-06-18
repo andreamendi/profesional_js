b634a3ae9387c9631295330a9947b166ddd79aa6
AndreaMendi <andreamendi@icloud.com>
2020-06-17 22:40:36 -0500

    Se realizo un plugin de mute/Unmute -> que hace switch

--- assets/MediaPlayer.js
+++ assets/MediaPlayer.js
@@ -1,7 +1,15 @@
 function MediaPlayer(config) {
-  this.media = config.el
+  this.media = config.el;
+  this.plugins = config.plugins || [];
+
+  this._initPlugins();
 };
 
+MediaPlayer.prototype._initPlugins = function() {
+  this.plugins.forEach(plugin => {
+    plugin.run(this)
+  });
+};
 MediaPlayer.prototype.play = function() {
   this.media.play();
 };
@@ -12,14 +20,54 @@ MediaPlayer.prototype.pause = function() {
 
 MediaPlayer.prototype.control = function() {
   if(this.media.paused) {
-    player.play();
+    this.play();
     let div = document.getElementById("PlayPause");
     div.innerHTML = "Pause";
   } else {
-    player.pause();
+    this.pause();
     let div = document.getElementById("PlayPause");
     div.innerHTML = "Play";
   }
 };
 
+MediaPlayer.prototype.controlSound = function() {
+  if(this.media.muted) {
+    this.unmute()
+    console.log('mute')
+    let div = document.getElementById("muteUnmute");
+    div.innerHTML = "Mute";
+  } else {
+    this.mute()
+    console.log('unmute')
+    let div = document.getElementById("muteUnmute");
+    div.innerHTML = "Unmute";
+  }
+}
+MediaPlayer.prototype.mute = function() {
+  this.media.muted = true
+}
+
+MediaPlayer.prototype.unmute = function() {
+  this.media.muted = false
+}
+
+// MediaPlayer.prototype.controlSound = function() {
+//   if(this.media.muted) {
+//     this.unmute()
+//     let div = document.getElementById("muteUnmute");
+//     div.innerHTML = "Muted";
+//   } else {
+//     this.mute()
+//     let div = document.getElementById("muteUnmute");
+//     div.innerHTML = "Unmuted";
+//   }
+// };
+
+// MediaPlayer.prototype.mute = function() {
+//   this.media.muted = true;
+// };
+
+// MediaPlayer.prototype.unmute = function() {
+//   this.media.unmuted = false;
+// };
 export default MediaPlayer;
--- assets/index.js
+++ assets/index.js
@@ -1,7 +1,20 @@
 import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video });
+const player = new MediaPlayer({ el: video, plugins: [
+  new AutoPlay()
+] });
 
-const button = document.querySelector('button');
-button.onclick = () => player.control();
+const buttonPlay = document.querySelector('#\\PlayPause ');
+buttonPlay.onclick = () => player.control();
+
+const buttonSound = document.querySelector('#\\muteUnmute ');
+buttonSound.onclick = () => player.controlSound();
+
+
+
+
+
+
