3e33144c3e0e97a849fcb36d3719e2794601a418
Andrea Mendi <andreamendi25@gmail.com>
2020-12-09 15:56:15 -0600

    Se eliminaron archivos "._", y se anexaron ejercicos de fetch, generators y ejercicios de typescrip

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,16 @@
 import MediaPlayer from './MediaPlayer.js';
 import AutoPlay from './plugins/AutoPlay.js';
+import AutoPause from './plugins/AutoPause.js';
 
 
 const video = document.querySelector('video');
-const player = new MediaPlayer({ el: video, plugins: [
-  new AutoPlay()
-] });
+const player = new MediaPlayer({
+  el: video,
+  plugins: [
+    new AutoPlay(),
+    new AutoPause()
+  ]
+});
 
 const buttonPlay = document.querySelector('#\\PlayPause ');
 buttonPlay.onclick = () => player.control();
@@ -13,7 +18,11 @@ buttonPlay.onclick = () => player.control();
 const buttonSound = document.querySelector('#\\muteUnmute ');
 buttonSound.onclick = () => player.controlSound();
 
-
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').catch(error => {
+    console.error(error.message);
+  })
+}
 
 
 
